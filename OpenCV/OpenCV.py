5f20b81deb75a15fc41eaae52d0f70b9ec99b016
刘念 <liunian63@outlook.com>
2019-03-11 23:00:36 +0800

    Harris角点检测

--- OpenCV/OpenCV.py
+++ OpenCV/OpenCV.py
@@ -12,15 +12,15 @@ im=array(pil_im)
 
 im2,cdf=CVtools.histeq(im)
 
-#im2=255-im#反相处理
+im2=255-im#反相处理
 
 #figure()#创建一个图像
 #gray()#不使用颜色信息
 #contour(im,origin='image')#在原点的左上角显示
 #im2=filters.gaussian_filter(im,10) #高斯模糊
 imshow(im2)#绘制图像
-#figure()
-#hist(im.flatten(),128)#直方图
+figure()
+hist(im.flatten(),128)#直方图
 
 
 
